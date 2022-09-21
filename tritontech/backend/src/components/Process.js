185f73ed836494df09cc7fedb97c76ddfce0c0a3
jalalmhd <JalalN@tritonlabs.in>
2022-09-21 13:22:53 +0530

    process_web

--- tritontech/backend/src/components/Process.js
+++ tritontech/backend/src/components/Process.js
@@ -1,23 +1,102 @@
 import React from 'react'
 // import '../Styles/Process.css';
 import '../Styles/Process.css';
+import {  Typography } from '@material-ui/core';
+// import img4 from '../images/3dobj2.png';
+// import img5 from '../images/3dobj2.png';
+// import img6 from '../images/3dobj2.png';
+// import img7 from '../images/3dobj2.png';
+import img4 from '../images/3dicons.png'
+import img5 from '../images/share-2.png'
+import img6 from '../images/cloud-development.png'
+import img7 from '../images/icon3.png'
+import {BsArrowDownCircleFill,BsDot} from 'react-icons/bs'
+import {BiBasketball} from 'react-icons/bi'
+import {ImArrowRight2} from 'react-icons/im'
+import {FaDigitalOcean} from 'react-icons/fa'
+import {BiDotsVerticalRounded} from 'react-icons/bi'
+import {MdHealthAndSafety} from 'react-icons/md'
+
 
 function Process() {
   return (
     <div className='Process'>
         
-     <div className='processer-content-hr'>
+        <div className='processer-content-hr'>
+        <div className='About-content-empty-header' >
+      </div>
+      <div className='Process-main'>
         
-            <div className='About-content-empty-header' style={{ background: '' }}>
-                
-            </div>
-            <div className='Process-main'>
-                
-              <div className='content-circle'>
+            <div className='content-circle'>
+                 <div className='process-circle' style={{background:'#ffffff'}}>
+                   <img src={img4} alt='' style={{width:'50%',height:'50%'}}/>
 
-              </div>
+                 </div>
+                 <div className='process-circle' style={{background:'#ffffff'}}>
+                 <img src={img5} alt='' style={{width:'50%',height:'50%'}}/>
             </div>
-          </div>
+         <div className='process-circle'style={{background:'#ffffff'}}>
+         <img src={img6} alt='' style={{width:'50%',height:'50%'}}/>
+           </div>
+           <div className='process-circle' style={{background:'#ffffff'}}>
+           <img src={img7} alt='' style={{width:'50%',height:'50%'}}/>
+             </div>
+            </div>
+            <div className='process-icon'>
+               <div className='process-icon-content'>
+                <BsDot size={40}/>
+                     <BiDotsVerticalRounded size={30} />
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+               </div>
+               <div className='process-icon-content'>
+               <BsDot size={40}/>
+                     <BiDotsVerticalRounded size={30} />
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+</div>
+<div className='process-icon-content'>
+<BsDot size={40}/>
+                     <BiDotsVerticalRounded size={30} />
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+</div>
+<div className='process-icon-content'>
+<BsDot size={40}/>
+                     <BiDotsVerticalRounded size={30} />
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+                     <BiDotsVerticalRounded size={30}/>
+
+</div>
+            </div>
+            <div className='process-heading'>
+               <Typography variant='h6' style={{marginRight:'60px'}} >
+                Discovery & Analysis
+               </Typography>
+               <Typography variant='h6' style={{marginRight:'60px'}}>
+               Architecture Design
+               </Typography>
+               <Typography variant='h6' style={{marginRight:'60px'}}>
+               Development Phase
+               </Typography>
+               <Typography variant='h6'style={{marginRight:'60px'}}>
+                Deployment
+               </Typography>
+            </div>
+           </div>
+        </div>
         </div>
   )
 }
