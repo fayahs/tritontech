9fdf1c7eea2f8fb849eeb1115155251d8ea7f0cc
jalalmhd <JalalN@tritonlabs.in>
2022-09-25 20:16:37 +0530

    name changing

--- tritontech/backend/src/components/Process.js
+++ tritontech/backend/src/components/Process.js
@@ -76,8 +76,8 @@ function Process() {
             </div>
             <div className='process-heading'>
                <Typography variant='h6' style={{marginRight:'60px'}} >
-                Discovery & Analysis
-               </Typography> 
+               Discovery & Analysis
+               </Typography>
                <Typography variant='h6' style={{marginRight:'60px'}}>
                Architecture Design
                </Typography>
