11babf998fd45560c21d095c34deff92b93403c3
Balanand KS <balanand.ks@epicastudio.onmicrosoft.com>
2022-09-28 12:24:23 +0530

    changes

--- tritontech/backend/src/components/Process.js
+++ tritontech/backend/src/components/Process.js
@@ -1,96 +1,102 @@
 import React from 'react'
 // import '../Styles/Process.css';
 import '../Styles/Process.css';
-import {  Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 import img4 from '../images/3dicons.png'
 import img5 from '../images/share-2.png'
 import img6 from '../images/cloud-development.png'
 import img7 from '../images/icon3.png'
-import {BsDot} from 'react-icons/bs'
-import {BiDotsVerticalRounded} from 'react-icons/bi'
+import { BsDot } from 'react-icons/bs'
+import { BiDotsVerticalRounded } from 'react-icons/bi'
 
 
 function Process() {
   return (
     <div className='Process'>
-        
-        <div className='processer-content-hr'>
-        <div className='About-content-empty-header' >
-      </div>
-      <div className='Process-main'>
-        
-            <div className='content-circle'>
-                 <div className='process-circle' style={{background:'#ffffff'}}>
-                   <img src={img4} alt='' style={{width:'50%',height:'50%'}}/>
 
-                 </div>
-                 <div className='process-circle' style={{background:'#ffffff'}}>
-                 <img src={img5} alt='' style={{width:'50%',height:'50%'}}/>
-            </div>
-         <div className='process-circle'style={{background:'#ffffff'}}>
-         <img src={img6} alt='' style={{width:'50%',height:'50%'}}/>
-           </div>
-           <div className='process-circle' style={{background:'#ffffff'}}>
-           <img src={img7} alt='' style={{width:'50%',height:'50%'}}/>
-             </div>
-            </div>
-            <div className='process-icon'>
-               <div className='process-icon-content'>
-                <BsDot size={40}/>
-                     <BiDotsVerticalRounded size={30} />
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-               </div>
-               <div className='process-icon-content'>
-               <BsDot size={40}/>
-                     <BiDotsVerticalRounded size={30} />
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-</div>
-<div className='process-icon-content'>
-<BsDot size={40}/>
-                     <BiDotsVerticalRounded size={30} />
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-</div>
-<div className='process-icon-content'>
-<BsDot size={40}/>
-                     <BiDotsVerticalRounded size={30} />
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
-                     <BiDotsVerticalRounded size={30}/>
+        <div className='process-main'>
+     
+        {/* <div className='About-content-empty-header' >
+        </div> */}
+        <div className='Process-img'>
+        </div>
 
-</div>
+          <div className='content-circle'>
+            <div className='cir-1'>
+              <div className='process-circle' style={{ background: '#ffffff' }}>
+                <img src={img4} alt='' style={{ width: '50%', height: '50%' }} />
+              </div>
+              <div className='process-icon-content'>
+                <BsDot size={40} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+              </div>
+              <Typography variant='h6'  >
+              Student Studies & Discovery Analysis
+              </Typography>
+              
             </div>
-            <div className='process-heading'>
-               <Typography variant='h6' style={{marginRight:'10px',color:'#777777'}} >
-                Student Studies &Discovery  Analysis
-               </Typography>
-               <Typography variant='h6' style={{marginRight:'120px',color:'#777777'}}>
-               Architecture Design
-               </Typography>
-               <Typography variant='h6' style={{marginRight:'120px',color:'#777777'}}>
-               Development & QA
-               </Typography>
-               <Typography variant='h6'style={{marginRight:'120px',color:'#777777'}}>
+            <div className='cir-1'>
+            <div className='process-circle' style={{ background: '#ffffff' }}>
+              <img src={img5} alt='' style={{ width: '50%', height: '50%' }} />
+            </div>
+            <div className='process-icon-content'>
+                <BsDot size={40} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+              </div>
+              <Typography variant='h6' >
+                Architecture Design
+              </Typography>
+            </div>
+            <div className='cir-1'>
+            <div className='process-circle' style={{ background: '#ffffff' }}>
+              <img src={img6} alt='' style={{ width: '50%', height: '50%' }} />
+            </div>
+            <div className='process-icon-content'>
+                <BsDot size={40} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+              </div>
+              <Typography variant='h6' >
+                Development & QA
+              </Typography>
+            </div>
+            <div className='cir-1'>
+            <div className='process-circle' style={{ background: '#ffffff' }}>
+              <img src={img7} alt='' style={{ width: '50%', height: '50%' }} />
+            </div>
+
+            <div className='process-icon-content'>
+                <BsDot size={40} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+                <BiDotsVerticalRounded size={30} />
+              </div>
+              <Typography variant='h6' >
                 Release
-               </Typography>
+              </Typography>
             </div>
-           </div>
-        </div>
-        </div>
+          </div>
+       
+          </div>
+    </div>
   )
 }
 
