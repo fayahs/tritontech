aa661af08fdc6fd9a67779505c4f6b193b2fca89
jalalmhd <JalalN@tritonlabs.in>
2022-09-30 00:09:47 +0530

    service and footer

--- tritontech/backend/src/components/Contact.js
+++ tritontech/backend/src/components/Contact.js
@@ -5,21 +5,20 @@ import img16 from '../images/location.png'
 import { Button, Typography } from '@material-ui/core';
 import img18 from '../images/ringer-volume.png'
 import { BsFacebook } from 'react-icons/bs';
-import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai';
 function Contact() {
   return (
     <div className="Contact-main">
-      <div className='About-content-empty-header'   >
-      </div>
+      
       <div className='Contact-main-hr'>
         <div className='Contacts-headers'>
           {/* <Typography>
        Magnam dolores commodi suscipit.
        </Typography> */}
           <Typography variant='h5' className='title' style={{color:'#777777'}}>
-            Want to discuss your <span style={{ fontWeight: '550',color:'#007FFF' }}>Project?</span>
+            Want to discuss your  <span style={{ fontWeight: '550',color:'#007FFF' }}>Project?</span>
           </Typography>
-          <Button style={{ textTransform: 'none', fontSize: '24px', color: 'black',color:'#007FFF' }} variant='text'>
+          <Button style={{ textTransform: 'none', fontSize: '24px',color:'#007FFF' }} variant='text'>
             Contact Us
           </Button>
 
@@ -28,21 +27,18 @@ function Contact() {
         <div className='Contacts-content'>
           <div className='Contact-box'>
             <div className='location-img'>
-              <img src={img16} alt='' style={{ width: '40%', height: 'auto' }} />
+              <img src={img16} alt='' style={{ width: '35%', height: 'auto' }} />
             </div>
-            <div className='location-content'>
+            <div className='location-content'   >
               <Typography variant='h5' className='con-head' style={{color:'#007FFF'}}>
                 Our Address
               </Typography>
-              <Typography  style={{display:'flex',alignItems:'center',flexDirection:'column',color:'#777777'}}>
+              <Typography  style={{display:'flex',alignItems:'center',flexDirection:'column',color:'#777777'}}  className='para2'>
                 
-              <span> 219, First Floor, Main Road,</span> 
+              <span > 219, First Floor, Main Road,</span> 
               
-              
-              <span>Kadayanallur,Thenkasi,</span>  
-             
-             
-               <span>Tamilnadu ,India</span> 
+              <span >Kadayanallur,Thenkasi,</span>    
+               <span >Tamilnadu ,India</span> 
          
 
 
@@ -54,7 +50,7 @@ function Contact() {
           </div>
           <div className='Contact-box'>
             <div className='location-img'>
-              <img src={img17} alt='' style={{ width: '40%', height: 'auto' }} />
+              <img src={img17} alt='' style={{ width: '35%', height: 'auto' }} />
             </div>
             <div className='location-content' >
               <Typography variant='h5' className='con-head' style={{color:'#007FFF'}} >
@@ -71,7 +67,7 @@ function Contact() {
           </div>
           <div className='Contact-box'>
             <div className='location-img'>
-              <img src={img18} alt='' style={{ width: '40%', height: 'auto' }} />
+              <img src={img18} alt='' style={{ width: '35%', height: 'auto' }} />
             </div>
             <div className='location-content' >
               <Typography variant='h5' className='con-head' style={{color:'#007FFF'}}>
@@ -90,72 +86,80 @@ function Contact() {
         </div>
         <div className='Contacts-content1'>
           <div className='Contacts-content2'>
-            <Typography variant='h6' className='last-con' style={{color:'#007FFF'}} >
+            <Typography variant='h6' className='last-con' style={{color:'#000000',fontWeight:'700'}} >
               MENU
             </Typography>
-            <a href='#sec2'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec2' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Home
             </Typography> </a>
-            <a href='#sec2'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec2' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               About us
             </Typography> </a>
-            <a href='#sec3'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec3' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Process
             </Typography> </a>
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4'className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Services
             </Typography> </a>
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Terms Of service
             </Typography> </a>
 
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Privacy Policy
             </Typography> </a>
 
           </div>
-          <div className='Contacts-content2'>
-            <Typography variant='h6'  className='last-con' style={{color:'#007FFF'}}>
+          <div className='Contacts-content2' >
+            <Typography variant='h6'  className='last-con' style={{color:'#000000',fontWeight:'700'}}>
               SERVICES
             </Typography>
-            <a href='#sec2'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec2' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Web Design
             </Typography> </a>
-            <a href='#sec2'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec2'className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Web Development
 
             </Typography> </a>
-            <a href='#sec3'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec3'className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Product Management
 
             </Typography> </a>
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Marketing
 
             </Typography> </a>
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Terms Of service
             </Typography> </a>
 
-            <a href='#sec4'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
+            <a href='#sec4'className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '12px' }}>
               Graphic Design
             </Typography> </a>
 
           </div>
-          <div className='Contacts-content2'>
-            <Typography variant='h6' className='last-con' style={{ fontSize: '20px',color:'#007FFF' }} >
+          <div className='Contacts-content2' >
+            <Typography variant='h6' className='last-con' style={{ fontSize: '20px',color:'#000000',fontWeight:'700',marginLeft:'28px' }} >
               SOCIAL
             </Typography>
-            <a href='#sec2'><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
-              <span >< AiFillTwitterCircle size={14} /></span><span style={{ marginLeft: '2px' }}>Twitter</span>
+            <div style={{display:'flex',alignItems:'center',justifyContent:'space-evenly',width:'50% '}}>
+            <a href='#sec2' className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
+              <span >< AiFillTwitterCircle size={30} /></span>
             </Typography > </a>
-            <a href='https://www.facebook.com/profile.php?id=100076158587804' style={{ display: 'flex' }}><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
-              <span >< BsFacebook size={12} /></span><span style={{ marginLeft: '2px' }}>Facebook</span>
+            <a href='https://www.facebook.com/profile.php?id=100076158587804' style={{ display: 'flex' }} className='link1'><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
+              <span >< BsFacebook size={30} /></span>
 
             </Typography> </a>
+            </div>
+            <div style={{display:'flex',alignItems:'center',justifyContent:'space-evenly',width:'50% '}}>
             <a href='https://www.linkedin.com/company/triton-tech-labs/'><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
-              <span >< AiFillLinkedin size={12} /></span><span style={{ marginLeft: '2px' }}>Linkedin</span>
+              <span >< AiFillLinkedin size={30} color='grey '/></span>
             </Typography> </a>
+            <a href='https://www.linkedin.com/company/triton-tech-labs/'><Typography variant='h6' component='h2' style={{ fontSize: '14px' }}>
+              <span >< AiFillInstagram size={30} color='grey' /></span>
+            </Typography> </a>
+            </div>
+            
 
 
           </div>
